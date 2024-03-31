3aa55b7e6040585b9a1847b5e84c8fb6761feb17
curtis clayton <curtisclayton4270@gmail.com>
2024-03-31 16:21:15 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,59 @@
+class Player {
+    constructor(game) {
+        this.game = game;
+        this.width = 100;
+        this.height = 100;
+        this.x = this.game.width * 0.5 - this.width * 0.5;
+        this.y = this.game.height - this.height;
+        this.speed = 0;
+    }
+    draw(context) {
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+    update(){
+        this.x += this.speed;
+    }
+}
+
+class Projectile {
+
+}
+
+class Enemy {
+
+}
+
+class Game {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.width = this.canvas.width;
+        this.height =this.canvas.height;
+        this.player = new Player(this);
+
+        // Event listeners
+        window.addEventListener("keydown", function(e){
+            console.log(e)
+        })
+    }
+    // Render will run 60 times per sec drawing a updating everything
+    render(context){
+      this.player.draw(context)
+      this.player.update();
+    }
+}
+
+window.addEventListener('load', function() {
+    const canvas = document.getElementById("canvas1");;
+    const ctx = canvas.getContext("2d");
+    canvas.width = 600;
+    canvas.height = 800;
+
+    const game = new Game(canvas)
+  
+    function animate() {
+        ctx.clearRect(0, 0 ,canvas.width, canvas.height)
+        game.render(ctx)
+        requestAnimationFrame(animate)
+    } 
+    animate()   
+})
