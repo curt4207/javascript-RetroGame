0339ee079cf0571cd25d4d07853f2b2db2bfb9b8
curtis clayton <curtisclayton4270@gmail.com>
2024-04-03 23:32:32 -0400

    Save in a wroking state, added player and enemy grid

--- script.js
+++ script.js
@@ -16,15 +16,19 @@ class Player {
         if( this.game.keys.indexOf("ArrowRight") > -1) this.x += this.speed; 
 
         // Horizontal movement Boundaries
-        if(this.x < 0) this.x = 0;
-        else if( this.x > this.game.width - this.width) this.x =this.game.width - this.width;
+        if(this.x < -this.width * 0.5) this.x = -this.width * 0.5;
+        else if( this.x > this.game.width - this.width * 0.5) this.x =this.game.width - this.width * 0.5;
         
+    }
+    shoot(){
+        const projectile = this.game.getProjectile();
+        if(projectile) projectile.start(this.x + this.width * 0.5, this.y);
     }
 }
 
 class Projectile {
  constructor(canvas){
-    this.width = 4;
+    this.width = 8;
     this.height = 20;
     this.x = 0;
     this.y = 0;
@@ -39,21 +43,76 @@ class Projectile {
   update(){ 
     if(!this.free) {
         this.y -= this.speed;
+        if(this.y < -this.height) this.reset();
     }
   }
-  start(){
-
+  start(x, y){
+    this.x = x - this.width * 0.5;
+    this.y = y;
+    this.free = false;
   }
 
   reset(){
-    
+    this.free = true;
   }
 }
 
 class Enemy {
-
+    constructor(game, positionX , positionY) {
+        this.game = game;
+        this.width = this.game.enemySize;
+        this.height = this.game.enemySize;
+        this.x = 0;
+        this.y = 0;
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+    draw(context) {
+        context.strokeRect(this.x, this.y, this.width, this.height);
+    }
+    update(x,y){
+        this.x = x + this.positionX;
+        this.y = y + this.positionY;
+    }
 }
+class Wave {
+    constructor(game) {
+        this.game = game;
+        this.width = this.game.columns * this.game.enemySize;
+        this.height  = this.game.rows * this.game.enemySize;
+        this.x = 0;
+        this.y = -this.height;
+        this.speedX = 3;
+        this.speedY = 0;
+        this.enemies = [];
+        this.create();
 
+    }
+    render(context){
+        if(this.y < 0) this.y += 5;
+        this.speedY = 0;
+        if(this.x < 0 ||  this.x > this.game.width - this.width) {
+            this.speedX *= -1;
+            this.speedY = this.game.enemySize;
+        }
+        this.x += this.speedX;
+        this.y += this.speedY;
+        this.enemies.forEach(enemy => {
+            enemy.update( this.x, this.y);
+            enemy.draw(context);
+        })
+
+    }
+    create() {
+        for(let y = 0; y < this.game.rows; y++) {
+            for(let x = 0; x < this.game.columns; x++) {
+                let enemyX = x * this.game.enemySize;
+                let enemyY = y * this.game.enemySize;
+                this.enemies.push(new Enemy(this.game, enemyX, enemyY))
+            }
+        }
+    }
+}
 class Game {
     constructor(canvas) {
         this.canvas = canvas;
@@ -63,10 +122,21 @@ class Game {
         this.keys = [];
         this.player = new Player(this);
 
+        this.projectilesPool = [];
+        this.numberOfProjectiles = 10;
+        this.createProjectiles()
+        
+        this.columns = 3;
+        this.rows = 3;
+        this.enemySize = 60;
+
+        this.waves = [];
+        this.waves.push(new Wave(this))
+
         // Event listeners
         window.addEventListener("keydown", e => {
         if(this.keys.indexOf(e.key) === -1) this.keys.push(e.key)
-           
+           if(e.key === "1") this.player.shoot();
     })
         window.addEventListener("keyup", e => {
         const index = this.keys.indexOf(e.key)
@@ -78,6 +148,26 @@ class Game {
     render(context){
       this.player.draw(context)
       this.player.update();
+      this.projectilesPool.forEach(projectile => {
+        projectile.update();
+        projectile.draw(context);
+      })
+
+      this.waves.forEach(wave => {
+        wave.render(context)
+      })
+    }
+    // create projectiles
+    createProjectiles(){
+        for(let i = 0; i < this.numberOfProjectiles; i++) {
+            this.projectilesPool.push(new Projectile());
+        }
+    }
+    // get free projectile object from the pool
+    getProjectile(){
+        for(let i = 0; i < this.projectilesPool.length; i++){
+            if(this.projectilesPool[i].free) return this.projectilesPool[i];
+        }
     }
 }
 
@@ -86,6 +176,9 @@ window.addEventListener('load', function() {
     const ctx = canvas.getContext("2d");
     canvas.width = 600;
     canvas.height = 800;
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 5;
 
     const game = new Game(canvas)
   
